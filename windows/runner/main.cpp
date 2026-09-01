92fc9f2549780e8803f9d7ddc50df4134cc95702
hanifnugrahaa <hanifardiyanta11@gmail.com>
2026-09-01 19:40:19 +0700

    style(mobile): resize Windows desktop simulator to compact 90% smartphone ratio (370x756)

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(120, 40);
-  Win32Window::Size size(410, 840);
+  Win32Window::Point origin(120, 30);
+  Win32Window::Size size(370, 756);
   if (!window.Create(L"G-Connect Mobile Simulator", origin, size)) {
     return EXIT_FAILURE;
   }
